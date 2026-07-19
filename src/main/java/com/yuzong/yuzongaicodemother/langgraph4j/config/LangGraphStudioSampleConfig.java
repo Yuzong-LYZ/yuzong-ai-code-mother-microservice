e3e2e0e6dbdbb3ffc243430d1c2d4ed2c2e8b359
Yuzong-LYZ <42740485+Yuzong-LYZ@users.noreply.github.com>
2026-07-19 17:22:05 +0800

    九、AI工作流（可视化交互和调试） 1. 整合studio（不好用）

---
+++ src/main/java/com/yuzong/yuzongaicodemother/langgraph4j/config/LangGraphStudioSampleConfig.java
@@ -0,0 +1,33 @@
+package com.yuzong.yuzongaicodemother.langgraph4j.config;
+
+import com.yuzong.yuzongaicodemother.langgraph4j.CodeGenWorkflow;
+import org.bsc.langgraph4j.GraphStateException;
+import org.bsc.langgraph4j.studio.springboot.AbstractLangGraphStudioConfig;
+import org.bsc.langgraph4j.studio.springboot.LangGraphFlow;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 可视化调试配置：
+ *
+ */
+@Configuration
+public class LangGraphStudioSampleConfig extends AbstractLangGraphStudioConfig {
+
+    final LangGraphFlow flow;
+
+    public LangGraphStudioSampleConfig() throws GraphStateException {
+        // 1. 把你想要可视化的工作流创建出来编译后得到状态图
+        var workflow = new CodeGenWorkflow().createWorkflow().stateGraph;
+        // 2. 得到状态图后，构造一个LangGraphFlow的builder。把图转化为一个LangGraphFlow对象可展示的流程图
+        this.flow = LangGraphFlow.builder()
+                .title("LangGraph Studio")
+                .stateGraph(workflow)
+                .build();
+    }
+
+    // 3. 重写getFlow方法，返回你构造的LangGraphFlow对象。前端拿到这个对象后，即可展示为流程图。
+    @Override
+    public LangGraphFlow getFlow() {
+        return this.flow;
+    }
+}
