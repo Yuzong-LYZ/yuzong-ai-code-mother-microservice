a141b247a03b62b86ecdba7580f41269a39b1a7e
Yuzong-LYZ <42740485+Yuzong-LYZ@users.noreply.github.com>
2026-07-30 00:43:07 +0800

    十二、微服务改造（截图服务模块screenshot，注意：本次提交为中间态，影响了单体项目代码，不要单独使用当前提交） 1. 引入截图服务相关pom依赖 和 相关application配置 2. 迁移代码：    - utils/WebScreenshotUtils.java 网页截图工具类    - service/ScreenshotService.java 截图服务和实现类 3. 编写启动类

---
+++ yuzong-ai-code-mother-microservice/yuzong-ai-code-screenshot/src/main/java/com/yuzong/yuzongaicodemother/yuzongaiCodeScreenshotApplication.java
@@ -0,0 +1,11 @@
+package com.yuzong.yuzongaicodemother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class yuzongaiCodeScreenshotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(yuzongaiCodeScreenshotApplication.class, args);
+    }
+}
